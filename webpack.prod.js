84fcb672502e6caf0700a559a0b2810425198aa3
Jay Amano <jayamano57@MacBook-Pro-5.local>
2020-03-11 23:35:06 -0700

    testing node config

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,19 +7,13 @@ const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = merge(common, {
+const clientConfig = merge(common, {
   mode: "production",
-  entry: { server: "./server.js", main: "./src/js/index.js" },
-  node: {
-    __dirname: false
-  },
+  target: "web",
+  entry: { main: "./src/js/index.js" },
   output: {
-    filename: data => {
-      return data.chunk.name === "server"
-        ? "[name].min.js"
-        : "[name].[contentHash].bundle.js";
-    },
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contentHash].bundle.js"
   },
   optimization: {
     minimizer: [
@@ -57,4 +51,73 @@ module.exports = merge(common, {
       }
     ]
   }
+  //…
 });
+const serverConfig = {
+  mode: "production",
+  target: "node",
+  entry: { server: "./server.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].min.js"
+  },
+  node: {
+    __dirname: false,
+    child_process: "empty"
+  }
+};
+
+// module.exports = merge(common, {
+//   mode: "production",
+//   target: "node",
+//   entry: { server: "./server.js", main: "./src/js/index.js" },
+//   node: {
+//     __dirname: false
+//   },
+//   output: {
+//     filename: data => {
+//       return data.chunk.name === "server"
+//         ? "[name].min.js"
+//         : "[name].[contentHash].bundle.js";
+//     },
+//     path: path.resolve(__dirname, "dist")
+//   },
+//   optimization: {
+//     minimizer: [
+//       new OptimizeCssAssetsPlugin(),
+//       new TerserPlugin(),
+//       new HtmlWebpackPlugin({
+//         template: "./src/template.html",
+//         minify: {
+//           removeAttributeQuotes: true,
+//           collapseWhitespace: true,
+//           removeComments: true
+//         }
+//       })
+//     ]
+//   },
+//   plugins: [
+//     new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
+//     new CleanWebpackPlugin()
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: [MiniCssExtractPlugin.loader, "css-loader"]
+//       },
+//       {
+//         test: /\.m?js$/,
+//         exclude: /(node_modules|bower_components)/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ["@babel/preset-env"]
+//           }
+//         }
+//       }
+//     ]
+//   }
+// });
+
+module.exports = [clientConfig, serverConfig];
