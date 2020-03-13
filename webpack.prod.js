b82be30a99494e94f100348c2b1e9e212c3e2f9e
Jay Amano <jayamano57@MacBook-Pro-5.local>
2020-03-13 00:57:54 -0700

    dotenv webpack plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -68,7 +68,7 @@ const serverConfig = {
     __dirname: false
   },
   plugins: [
-    new Dotenv()
+    // new Dotenv()
     // new webpack.DefinePlugin({
     //   "process.env.NODE_EMAIL": JSON.stringify(process.env.NODE_EMAIL),
     //   "process.env.NODE_PASS": JSON.stringify(process.env.NODE_PASS)
