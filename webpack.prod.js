44ccdf90b01cbb04e5acad1bb543c07d6a616b63
Jay Amano <jayamano57@MacBook-Pro-5.local>
2020-03-13 00:24:12 -0700

    dotenv webpack plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const webpack = require("webpack");
 const Dotenv = require("dotenv-webpack");
-require("dotenv").config();
+// require('dotenv').config()
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
