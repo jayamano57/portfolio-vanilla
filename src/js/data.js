2d12a3cc5d4489a40f134eb07ce4fa39b3fe1439
Jay Amano <jayamano57@MacBook-Pro-5.local>
2020-04-02 00:33:51 -0700

    finished new color theme change

--- src/js/data.js
+++ src/js/data.js
@@ -31,8 +31,10 @@ export const data = {
         { url: babelImg, name: "babel" },
         { url: webpackImg, name: "webpack" }
       ],
-      shortDescription: "A short description for my personal site goes here",
-      description: "personal site description",
+      shortDescription:
+        "This completely vanilla site you are currently looking at. Mmm Yummy.",
+      description:
+        "What you see before your eyes is my personal portfolio website, made with 100% vanilla css, html, and javascript. No frameworks and no libraries -- well, apart from my mailer package adn webpack, but you get the idea. With this project, I not only wanted to showcase my pure css/html/js abilities, but also to challenge myself by stripping me of my tools and taking me back to the very basics, with good ol' document.querySelector. As far as my backend goes, I am using node.js and express.js to handle my email sending. Nothing fancy here, just one job. Lastly, I am using Webpack to bundle my files and spit out a production ready dist folder.",
       gitHub: "https://github.com/jayamano57/jayamano-2"
     },
     {
@@ -47,8 +49,10 @@ export const data = {
         { url: expressjsImg, name: "express.js" },
         { url: mssqlImg, name: "sql server" }
       ],
-      shortDescription: "A short description for sellers place goes here",
-      description: "sellers place description"
+      shortDescription:
+        "A SaaS application that enables craft vendors and event promoters to connect and engage online via a dynamic and global marketplace.",
+      description:
+        "Seller's Place is a SaaS application that helps event vendors and promoters to connect, but also it is also my first real application project I've worked on. As a full-stack developer for Seller's Place, my fingerprints are all over this project as I helped build many of the frontend web components, as well as helped create API's and stored procedures in the backend. As far as technologies, we worked in React.js in the frontend, Node.js and Microsoft SQL Server in the backend. As seen in the demo, some of the features I worked on in this project were the 'Invite Email' feature, the online payment service, transaction history log, product type management system, and the venue details page. The 'Invite Email' service uses SendGrid API to allow event promoters to reach out to specific vendors and send an invitation email to them directly. The online payment service utilizes Node Stripe API to ensure secure transactions. The transaction history log uses data regarding a specific user's payment history pulled from SQL database to create a paginated visual log of their transactions. Additionally, I Designed and developed the stored procedures, Node.js endpoints, and front-end services (using Axios) to receive this data. The Product Type Management System, was created for users to manage their product types on a single page. Editing for this page was done by inline editing component custom designed for convenience and better UX. Lastly, the Venue Details page was built to display a targeted venue. It uses the Google Maps API and react-geocode to render a map of the venue for user to view. Additionally, created stored procedures in SQL to grab data for the six most recent events of said venue."
     },
     {
       title: "McMacro",
@@ -64,8 +68,10 @@ export const data = {
         { url: jqueryImg, name: "jQuery" },
         { url: "", name: "Cheerio.js" }
       ],
-      shortDescription: "A short description for McMacro goes here",
-      description: "mcmacro app description",
+      shortDescription:
+        "Diet time! Get a list of popular restaurants and the items from each that fulfill your macronutrient needs.",
+      description:
+        "This is my webscraping project I built for days when I was too lazy to cook, but still want to reach my macronutrient goals for the day. I am using React.js as the frontend framework, an accordion component from Material-UI, and Chart.js to render the beautiful pie chart. For the data, I am using Cheerio.js in Node.js to scrape a very convinient website, fastfoodmacros.com. It's no surpise here that most of my application magic occurs in the Node.js/Express.js environemnt as I use user input data to filter through the scraped data, and return a list of restaurants and items that match the given criteria. I look forward to booting this baby up next January for my annual fitness resolution that lasts a few months.",
       gitHub: "https://github.com/jayamano57/mcmacro"
     },
     {
@@ -80,8 +86,10 @@ export const data = {
         { url: yelpImg, name: "Yelp API" },
         { url: "", name: "Material Kit" }
       ],
-      shortDescription: "A short description for my foodr goes here",
-      description: "foodr app description"
+      shortDescription:
+        "Ever hungry, but unsure what you're hungry for? Here's Foodr. Swipe left or right based on pictures of food from restaurants. No worries about failed right swipes here 😉",
+      description:
+        "This application was a product of an idea I had long before I had aspirations of becoming a developer. Essentially, it is Tinder, but for food. And the best part is, swiping right has a 100% match success rate. "
     }
   ]
 };
